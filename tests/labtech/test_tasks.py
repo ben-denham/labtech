790a7973991f4fc269b198a3564d03fb75377eb4
Nathan McDougall <nathan.j.mcdougall@gmail.com>
2024-04-10 09:32:56 +1200

    Use a square brackets for indexing in messages for immatability issues
    Signed-off-by: Nathan McDougall <nathan.j.mcdougall@gmail.com>

--- tests/labtech/test_tasks.py
+++ tests/labtech/test_tasks.py
@@ -1,8 +1,17 @@
+import re
 from enum import Enum
 
+import pandas as pd
 import pytest
 from frozendict import frozendict
+from labtech.exceptions import TaskError
 from labtech.tasks import immutable_param_value
+
+
+class _BadObject:
+    """An object which is not supported by `immutable_param_value`."""
+
+    pass
 
 
 class _ExampleEnum(Enum):
@@ -22,13 +31,13 @@ class TestImmutableParamValue:
 
     def test_dict(self) -> None:
         assert immutable_param_value("hello", {"a": 1, "b": 2}) == frozendict(
-            {"hello.a": 1, "hello.b": 2}
+            {"a": 1, "b": 2}
         )
 
     def test_frozendict(self) -> None:
         assert immutable_param_value(
             "hello", frozendict({"a": 1, "b": 2})
-        ) == frozendict({"hello.a": 1, "hello.b": 2})
+        ) == frozendict({"a": 1, "b": 2})
 
     def test_tuple(self) -> None:
         assert immutable_param_value("hello", (1, 2, 3)) == (1, 2, 3)
@@ -38,16 +47,19 @@ class TestImmutableParamValue:
 
     def test_nested_dict(self) -> None:
         assert immutable_param_value("hello", {"a": 1, "b": {"c": 2}}) == frozendict(
-            {"hello.a": 1, "hello.b": frozendict({"hello.b.c": 2})}
+            {"a": 1, "b": frozendict({"c": 2})}
         )
 
     def test_multiple_nesting(self) -> None:
         assert immutable_param_value("hello", {"a": [1, {"b": 2}]}) == frozendict(
-            {"hello.a": (1, frozendict({"hello.a.1.b": 2}))}
+            {"a": (1, frozendict({"b": 2}))}
         )
 
     def test_nested_list_dict(self) -> None:
-        assert immutable_param_value("hello", [1, {"a": 2}]) == (1, frozendict({"hello.1.a": 2}))
+        assert immutable_param_value("hello", [1, {"a": 2}]) == (
+            1,
+            frozendict({"a": 2}),
+        )
 
     @pytest.mark.parametrize(
         "value",
@@ -64,3 +76,10 @@ class TestImmutableParamValue:
     )
     def test_scalar(self, value) -> None:
         assert immutable_param_value("hello", value) is value
+
+    def test_multiple_nested_error(self) -> None:
+        match = re.escape(
+            """Unsupported type '_BadObject' in parameter value 'hello["b"][2]["c"]'."""
+        )
+        with pytest.raises(TaskError, match=match):
+            immutable_param_value("hello", {"a": 1, "b": (1, 2, {"c": _BadObject()})})
